446129468e4f98492585a5d6c21792792d3acd53
Biniyam <biniyamseid3@gmail.com>
2023-11-17 21:36:48 -0500

    feat:nav and footer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,37 @@
+import { hamburger } from "../assets/icons"
+import { headerLogo } from "../assets/images"
+import { navLinks } from "../constants"
 
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className=" padding-x py-8 w-full absolute  " >
+      <nav className="flex justify-between  ">
+        <a>
+          <img  className=""
+            src ={headerLogo}
+            height={175}
+            width={175}
+          />
+
+        </a>
+        <ul className="flex gap-20 padding-x max-lg:hidden "> 
+          {navLinks.map((item)=>(
+            <li key = {item.label}>
+              <a  className="text-black "> 
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <img className="hidden max-lg:block "
+        src={hamburger}
+        width={25}
+        height={25}
+       >
+        </img>
+      </nav>
+    </header>
   )
 }
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,8 +1,37 @@
+import { footerLogo } from "../assets/images";
+import { socialMedia } from "../constants";
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div>
+        <div>
+          <div>
+            <img
+            src={footerLogo} 
+            height={100}
+            width={100}>
+            </img>
+          </div>
+          <div>
+          Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </div>
+          <div id="" className="flex mt-8 items-center gap-5 ">
+          {socialMedia.map((icon) => (
+              <div
+                className="flex bg-white h-12 w-12 gap-6 rounded-full justify-center"
+                key={icon.alt}
+              >
+                <img className="" src={icon.src} alt={icon.alt} width={24} height={24} />
+              
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
